026c3ff8a87e7caeac0f31273684c35a630774d3
Gabriel Rengifo <gabo.dev@outlook.com>
2020-10-03 19:32:57 -0500

    Remove credentials and upload presentantion

--- avaya.js
+++ avaya.js
@@ -2,13 +2,13 @@ var cpaas = require('@avaya/cpaas');
 var enums = cpaas.enums;
 
 var connector = new cpaas.SmsConnector({
-    accountSid: 'AC777c3e322e90e9b30f694ef38848d089',
-    authToken: 'e8bc0b7e16df4ad3b0e5209329a68f8b'
+    accountSid: 'TU_accountSid',
+    authToken: 'TU_authToken'
 });
 
 //send sms message
 connector.sendSmsMessage({
-    to: '+573113696125',
+    to: 'Numero_telefonico',
     from: '+654321',
     body: 'Hello from Avaya CPaaS!',
     statusCallback: 'http://mycallback.url.com',
--- ringout.js
+++ ringout.js
@@ -1,54 +1,13 @@
-// const SDK = require('@ringcentral/sdk').SDK
-
-// RECIPIENT = '+573113696125'
-
-// RINGCENTRAL_CLIENTID = 'yAyKW4G7RpGv74-qnY8Plg'
-// RINGCENTRAL_CLIENTSECRET = '0X727UP3RM-HMolq7yopDg3lF8NZRaTw6jt4FiGdtdIw'
-// RINGCENTRAL_SERVER = 'https://platform.devtest.ringcentral.com'
-
-// RINGCENTRAL_USERNAME = '+14244276848'
-// RINGCENTRAL_PASSWORD = 'creatic2020!'
-// RINGCENTRAL_EXTENSION = '101'
-
-// var rcsdk = new SDK({
-//     server: RINGCENTRAL_SERVER,
-//     clientId: RINGCENTRAL_CLIENTID,
-//     clientSecret: RINGCENTRAL_CLIENTSECRET
-// });
-// var platform = rcsdk.platform();
-// platform.login({
-//   username: RINGCENTRAL_USERNAME,
-//   password: RINGCENTRAL_PASSWORD,
-//   extension: RINGCENTRAL_EXTENSION
-// })
-// .then(function(resp) {
-//     call_ringout()
-// });
-
-// function call_ringout() {
-//     platform.post('/restapi/v1.0/account/~/extension/~/ring-out', {
-//       'from' : { 'phoneNumber': RINGCENTRAL_USERNAME },
-//       'to'   : {'phoneNumber': RECIPIENT},
-//       'playPrompt' : false
-//     })
-//     .then(function(resp) {
-//       return resp.json()
-//     })
-//     .then(function(json){
-//         console.log("Call placed. Call status: " + json.status.callStatus)
-//     })
-// }
-
 const SDK = require('@ringcentral/sdk').SDK
 
-RECIPIENT = '+573113696125'
+RECIPIENT = 'NUMERO_A_LLAMAR'
 
-RINGCENTRAL_CLIENTID = 'FMW3kAsiTf6qxLmhbFGDow'
-RINGCENTRAL_CLIENTSECRET = 'VW7byPS0TmyTggSMivMXcg37WHSq0WQnS7lj6_DGkl5A'
+RINGCENTRAL_CLIENTID = 'TU_CLIENTID'
+RINGCENTRAL_CLIENTSECRET = 'TU_CLIENTSECRET'
 RINGCENTRAL_SERVER = 'https://platform.devtest.ringcentral.com'
 
-RINGCENTRAL_USERNAME = '+14244276848'
-RINGCENTRAL_PASSWORD = 'creatic2020!'
+RINGCENTRAL_USERNAME = 'TU__USERNAME'
+RINGCENTRAL_PASSWORD = 'TU__PASSWORD'
 RINGCENTRAL_EXTENSION = '101'
 
 var rcsdk = new SDK({
@@ -58,21 +17,62 @@ var rcsdk = new SDK({
 });
 var platform = rcsdk.platform();
 platform.login({
-    username: RINGCENTRAL_USERNAME,
-    password: RINGCENTRAL_PASSWORD,
-    extension: RINGCENTRAL_EXTENSION
+  username: RINGCENTRAL_USERNAME,
+  password: RINGCENTRAL_PASSWORD,
+  extension: RINGCENTRAL_EXTENSION
+})
+.then(function(resp) {
+    call_ringout()
+});
+
+function call_ringout() {
+    platform.post('/restapi/v1.0/account/~/extension/~/ring-out', {
+      'from' : { 'phoneNumber': RINGCENTRAL_USERNAME },
+      'to'   : {'phoneNumber': RECIPIENT},
+      'playPrompt' : false
     })
     .then(function(resp) {
-        send_sms()
-    });
-
-function send_sms(){
-  platform.post('/restapi/v1.0/account/~/extension/~/sms', {
-       from: {'phoneNumber': RINGCENTRAL_USERNAME},
-       to: [{'phoneNumber': RECIPIENT}],
-       text: 'Hello World from JavaScript'
-     })
-     .then(function (resp) {
-        console.log("SMS sent. Message status: " + resp.json().messageStatus)
-     });
+      return resp.json()
+    })
+    .then(function(json){
+        console.log("Call placed. Call status: " + json.status.callStatus)
+    })
 }
+
+// const SDK = require('@ringcentral/sdk').SDK
+
+// RECIPIENT = '+573113696125'
+
+// RINGCENTRAL_CLIENTID = 'TU_CLIENTID'
+// RINGCENTRAL_CLIENTSECRET = 'TU_CLIENTSECRET'
+// RINGCENTRAL_SERVER = 'https://platform.devtest.ringcentral.com'
+
+// RINGCENTRAL_USERNAME = 'TU__USERNAME'
+// RINGCENTRAL_PASSWORD = 'TU__PASSWORD'
+// RINGCENTRAL_EXTENSION = '101'
+
+// var rcsdk = new SDK({
+//     server: RINGCENTRAL_SERVER,
+//     clientId: RINGCENTRAL_CLIENTID,
+//     clientSecret: RINGCENTRAL_CLIENTSECRET
+// });
+// var platform = rcsdk.platform();
+// platform.login({
+//     username: RINGCENTRAL_USERNAME,
+//     password: RINGCENTRAL_PASSWORD,
+//     extension: RINGCENTRAL_EXTENSION
+//     })
+//     .then(function(resp) {
+//         send_sms()
+//     });
+
+// function send_sms(){
+//   platform.post('/restapi/v1.0/account/~/extension/~/sms', {
+//        from: {'phoneNumber': RINGCENTRAL_USERNAME},
+//        to: [{'phoneNumber': RECIPIENT}],
+//        text: 'Hello World from JavaScript'
+//      })
+//      .then(function (resp) {
+//         console.log("SMS sent. Message status: " + resp.json().messageStatus)
+//      });
+// }
